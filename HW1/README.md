65303f93cd2fcd4b38ecc9cd7497fc5c9499e36f
Daniel <schwartzray8@gmail.com>
2022-09-09 10:46:53 -0400

    completed func

---
+++ HW1/README.md
@@ -0,0 +1,10 @@
+How to use:
+
+1. Clone github repo
+2. Open a live server from the index.html file
+3. Click the + to add a note
+4. Double click a note to delete it
+5. Copy URL to share with friends by clicking "Share notes" button
+
+Sources:
+• Code comes from: https://medium.com/@diogo.fg.pinheiro/simple-to-do-list-app-with-node-js-and-mongodb-chapter-1-c645c7a27583
--- HW1/index.js
+++ HW1/index.js
@@ -1,18 +1,77 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
+
+//models
+const TodoTask = require("./models/TodoTask");
 
 app.use("/static", express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
+//connection to db
+// !! Deprecated
+// mongoose.set("useFindAndModify", false);
+const db = mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true },
+  () => {
+    console.log("Connected to db!");
+    app.listen(3000, () => console.log("Server Up and running"));
+  }
+);
+
 app.set("view engine", "ejs");
 
+// GET METHOD
 app.get("/", (req, res) => {
-  res.render("todo.ejs");
+  TodoTask.find({}, (err, tasks) => {
+    res.render("todo.ejs", { todoTasks: tasks });
+  });
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
+// POST METHOD
+app.post("/", async (req, res) => {
+  console.log("BEFORE");
+  console.log(req.body.content);
+  const todoTask = new TodoTask({
+    content: req.body.content,
+  });
+  try {
+    await todoTask.save();
+    console.log("success");
+    res.redirect("/");
+  } catch (err) {
+    console.log("err", err);
+    res.redirect("/");
+  }
 });
 
-app.listen(3000, () => console.log("Server Up and running"));
+// UPDATE METHOD
+app
+  .route("/edit/:id")
+  .get((req, res) => {
+    const id = req.params.id;
+    TodoTask.find({}, (err, tasks) => {
+      res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+    });
+  })
+  .post((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndUpdate(id, { content: req.body.content }, (err) => {
+      if (err) return res.send(500, err);
+      res.redirect("/");
+    });
+  });
+
+// DELETE METHOD
+app.route("/remove/:id").get((req, res) => {
+  const id = req.params.id;
+  TodoTask.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+  });
+});
