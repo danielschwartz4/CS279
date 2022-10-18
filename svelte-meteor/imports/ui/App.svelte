86eca4fb1e8eb37cd08c25238d1803057485cada
Daniel <schwartzray8@gmail.com>
2022-10-18 12:32:14 -0400

    more

--- svelte-meteor/imports/ui/App.svelte
+++ svelte-meteor/imports/ui/App.svelte
@@ -4,23 +4,48 @@
   import TaskForm from './TaskForm.svelte';
 
   import { useTracker } from 'meteor/rdb:svelte-meteor-data';
-  $: tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 }}).fetch());
+  // $: tasks = useTracker(() => TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, { sort: { createdAt: -1 } }).fetch());
 
   console.log(tasks)
-//  
+  let hideCompleted = false;
+    
+  const setHideCompleted = value =>  {
+      hideCompleted = value;
+  }
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+ 
+  $: tasks = useTracker(() => TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, { sort: { createdAt: -1 } }).fetch());
+  $: incompleteCount = useTracker(() =>TasksCollection.find(hideCompletedFilter).count());
+
+  $: pendingTasksTitle = `${
+          incompleteCount ? ` (${$incompleteCount})` : ''
+  }`;
+  
 </script>
 
 
-<div class="container">
-  <header>
-    <h1>Todo List</h1>
-  </header>
+<div class="app">
+    <header>
+        <div class="app-bar">
+            <div class="app-header">
+                <h1>📝️ To Do List {pendingTasksTitle}</h1>
+            </div>
+        </div>
+    </header>
 
-  <TaskForm />
+    <div class="main">
+      <TaskForm />
+        <div class="filter">
+            <button on:click={() => setHideCompleted(!hideCompleted)}>
+                {hideCompleted ? 'Show All' : 'Hide Completed'}
+            </button>
+        </div>
 
-  <ul>
-    {#each $tasks as task (task._id)}
-        <Task task={task} />
-    {/each}
-  </ul>
+        <ul class="tasks">
+            {#each $tasks as task (task._id)}
+                <Task task={task} />
+            {/each}
+        </ul>
+    </div>
 </div>
